795a6a6fd83d5585c9369bf0498542877184fa6a
Harnisch.Victor <harnisch.victor@gmail.com>
2021-09-09 17:16:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/HarnischVictor" target="_blank" rel="noreferrer">Harnisch, Victor</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Projeto de Estudo e divulgação: <a href="https://www.linkedin.com/in/victor-harnisch/" target="_blank" rel="noreferrer">@harnisch</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
