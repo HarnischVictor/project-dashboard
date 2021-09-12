cd59489fc7530b3fb206427e09ce5618924a2ea3
Victor Harnisch <Harnisch Victor>
2021-09-12 14:04:05 -0300

    Pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,8 @@
-import BarChar from "components/BarChart";
-import DonutChart from "components/DonutChart";
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Planilha de Empresas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Percentual de Sucesso</h5>
-            <BarChar />
-
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
-            <DonutChart />
-
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as Vendas</h2>
-
-          </div>
-
-        <DataTable />
-
-      </div>
-      <Footer />
-    </>
+    <Routes />
   );
 }
 
