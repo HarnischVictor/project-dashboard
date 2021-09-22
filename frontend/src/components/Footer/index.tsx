053c1b7dd48e533ca8062787df3f865fda32fc8e
VICTOR HARNISCH <67118390+HarnischVictor@users.noreply.github.com>
2021-09-22 13:08:11 -0300

    correção do index
    correção do index footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -5,13 +5,9 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/HarnischVictor" target="_blank" rel="noreferrer">Harnisch, Victor</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Contato para Novos Projetos e contratação: <a href="https://www.linkedin.com/in/victor-harnisch/" target="_blank" rel="noreferrer">@LinkedIn</a></small></p>
-
+                    Projeto de Estudo e divulgação: <a href="https://www.linkedin.com/in/victor-harnisch/" target="_blank" rel="noreferrer">@harnisch</a></small></p>
             </div>
-            <div>
-            <p><small>
-                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p></div>
-        </footer >
+        </footer>
     );
 }
 
